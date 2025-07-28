2dd1829fe62aef7f57a8de93d4b8bf4712ea9522
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-07-28 23:18:13 +0200

    Implement "self on top" and "self only" display options
    Use same colour than arcdps when using profession/subgroup for the name colour.

--- src/State.h
+++ src/State.h
@@ -69,7 +69,9 @@ struct HealWindowOptions
 	bool ReplacePlayerWithAccountName = false;
 	bool UseProfessionForNameColour = false;
 	bool UseSubgroupForNameColour = false;
+	bool SelfOnTop = false;
 	bool HideSelfFromList = false;
+	bool SelfOnly = false;
 	char Name[MAX_HEAL_WINDOW_NAME + 1] = {};
 	char TitleFormat[MAX_HEAL_WINDOW_TITLE + 1] = "{1} ({4}/s, {7}s in combat)";
 	char EntryFormat[MAX_HEAL_WINDOW_ENTRY + 1] = "{1} ({4}/s, {7}%)";
